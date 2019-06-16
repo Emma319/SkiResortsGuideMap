3211d66142441db3a4ee7c58cbbd13127cd763cb
Emma.W <emmayt@outlook.com>
2019-06-16 00:29:47 -0700

    Updates

---
+++ precache-manifest.47ef66f8adc7a3220ced4f2eee0cfdd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b884e292f0f2f394e31cd93106b0386e",
+    "url": "/SkiResortsGuideMap/static/media/search.b884e292.svg"
+  },
+  {
+    "revision": "32cee4372a4c9744f7e703013278c61d",
+    "url": "/SkiResortsGuideMap/static/media/cancel.32cee437.svg"
+  },
+  {
+    "revision": "7f0a6b5d8d48334bf4bd",
+    "url": "/SkiResortsGuideMap/static/js/runtime~main.7f0a6b5d.js"
+  },
+  {
+    "revision": "25906404bb1d50d34765",
+    "url": "/SkiResortsGuideMap/static/js/main.25906404.chunk.js"
+  },
+  {
+    "revision": "f6f6ab01ca9411f30a69",
+    "url": "/SkiResortsGuideMap/static/js/1.f6f6ab01.chunk.js"
+  },
+  {
+    "revision": "25906404bb1d50d34765",
+    "url": "/SkiResortsGuideMap/static/css/main.e9b8e62f.chunk.css"
+  },
+  {
+    "revision": "da32731c7b9998fbc4ca2d8d4dfa45a3",
+    "url": "/SkiResortsGuideMap/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/SkiResortsGuideMap/precache-manifest.47ef66f8adc7a3220ced4f2eee0cfdd5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SkiResortsGuideMap/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
